7d1518ed33993bb921c872b75af9284fc380f9ba
Zenilda Pedrosa Vieira <101183963+ZenildaVieira@users.noreply.github.com>
2025-07-05 17:14:09 -0300

    codigo get_all_icons e get_icon_sprite - PTOSS-4

--- wagtail/admin/icons.py
+++ wagtail/admin/icons.py
@@ -7,6 +7,7 @@ from django.template.loader import render_to_string
 from django.urls import reverse
 
 from wagtail import hooks
+from pathlib import Path
 
 icon_comment_pattern = re.compile(r"<!--.*?-->", re.DOTALL)
 
@@ -28,6 +29,41 @@ def get_icons():
     return render_to_string(
         "wagtailadmin/shared/icons.html", {"icons": combined_icon_markup}
     )
+
+
+icon_comment_pattern = re.compile(r"<!--.*?-->", re.DOTALL)
+
+DEFAULT_ICONS = [
+    '<svg xmlns="http://www.w3.org/2000/svg"><path d="M-default"/></svg>',
+]
+
+def get_all_icons():
+    icons = DEFAULT_ICONS.copy()
+    for hook in hooks.get_hooks("register_icons"):
+        icons += hook([])  # ← ignora o conteúdo atual, apenas acumula
+    return icons
+
+
+def get_icon_sprite():
+    icons = get_all_icons()
+    combined = ""
+
+    for item in icons:
+        # Verifica se é um caminho de arquivo SVG
+        if isinstance(item, (str, Path)) and str(item).endswith(".svg") and Path(item).exists():
+            svg = Path(item).read_text(encoding="utf-8")
+        else:
+            svg = str(item)
+
+        # Limpeza e conversão para <symbol>
+        svg = svg.replace('xmlns="http://www.w3.org/2000/svg"', "")
+        svg = svg.replace("<svg", "<symbol").replace("</svg>", "</symbol>")
+        svg = icon_comment_pattern.sub("", svg)
+
+        combined += svg
+
+    return f'<svg xmlns="http://www.w3.org/2000/svg" style="display: none">{combined}</svg>'
+
 
 
 @lru_cache(maxsize=None)
